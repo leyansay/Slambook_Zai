86261c32a04e956a0d7343db7e7efc59f2e270db
diocs <diocsonleannzaira0@gmail.com>
2024-11-13 19:47:28 +0800

    slambook1

---
+++ app/src/main/java/com/example/slambook/Profile.kt
@@ -0,0 +1,27 @@
+package com.example.slambook
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Profile : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile)
+
+
+        findViewById<ImageView>(R.id.back).setOnClickListener {
+            startActivity(Intent(this, Home::class.java))
+            finish()
+        }
+    }
+}
